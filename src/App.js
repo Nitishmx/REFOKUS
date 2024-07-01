9c641d72826942a1cb368c0e496746329843795d
nitishmx <nitish7739879577@gmail.com>
2024-07-01 12:43:03 +0530

    product component complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import NavBar from './Component/NavBar'
 import Works from './Component/Works'
 import Stripes from './Component/Stripes'
+import Products from './Component/Products'
 
 function App() {
   return (
-    <div className='w-full h-screen font-["satoshi"] bg-zinc-800 text-white'>
+    <div className='w-full h-full font-["satoshi"] bg-zinc-800 text-white'>
      <NavBar/>
      <Works/>
      <Stripes/>
+     <Products/>
     </div>
   )
 }
---
+++ src/Component/Products.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Product from './Product'
+
+function Products() {
+    let products=[
+        {title:"arqitel",description:"With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into real estate.",live:true,case:false},
+        {title:"TTR",description:"With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into real estate.",live:true,case:false},
+        {title:"YIR 2022",description:"With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into real estate.",live:true,case:false},
+        {title:"Yahoo!",description:"With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into real estate.",live:true,case:false},
+    ]
+  return (
+    <div>
+    {products.map((item,index)=>{
+        return(
+        <Product value={item}/>
+
+        )
+    })}
+    </div>
+  )
+}
+
+export default Products
